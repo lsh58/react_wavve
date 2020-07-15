4b8ca635d40d74b653b46001b4ad314489659ea7
lsh58 <lsh58@gmail.com>
2020-07-15 17:15:40 +0900

    Updates

---
+++ precache-manifest.2e9341b4de8aa36dd774563eb044d1d6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "775457c64e1971e8e8bd29fcc6094774",
+    "url": "/react_wavve/index.html"
+  },
+  {
+    "revision": "4df7c7841ef27c90ea88",
+    "url": "/react_wavve/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4c0dc18ac559bc57bda4",
+    "url": "/react_wavve/static/js/2.e21fc2b0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_wavve/static/js/2.e21fc2b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4df7c7841ef27c90ea88",
+    "url": "/react_wavve/static/js/main.ac19cabd.chunk.js"
+  },
+  {
+    "revision": "f61abe193d768978eb43",
+    "url": "/react_wavve/static/js/runtime-main.9fe88798.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react_wavve/static/media/logo.5d5d9eef.svg"
+  }
+]);
