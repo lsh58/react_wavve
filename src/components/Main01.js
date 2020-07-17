857b76428bbd6c41bad5a4e2976a59c6b116d91d
lsh58 <lsh58@gmail.com>
2020-07-17 18:48:12 +0900

    update header
    파비콘 수정, banner이미지 추가, controlBtn수정

--- src/components/Main01.js
+++ src/components/Main01.js
@@ -6,7 +6,7 @@ const Main01 = () => {
     
     const slideWrapper = useRef(null);
     
-    var originLeft = -75;
+    var originLeft = 0;
     var bln =true;
 
     const onleftclick = () =>{
@@ -14,48 +14,74 @@ const Main01 = () => {
             bln=false;
             slideWrapper.current.prepend(slideWrapper.current.lastChild);
             slideWrapper.current.style.transition="all 0s ease 0s";
-            slideWrapper.current.style.left=(originLeft-50)+"%";
-            originLeft-=50;
+            slideWrapper.current.style.left=(originLeft-760)+"px";
+            originLeft-=760;
             setTimeout(function(){
                 slideWrapper.current.style.transition="all 0.35s ease 0s";
-                slideWrapper.current.style.left=(originLeft+50)+"%";
-                originLeft+=50;
+                slideWrapper.current.style.left=(originLeft+760)+"px";
+                originLeft+=760;
             },10);
             setTimeout(function(){
                 bln=true;
-            },200);
-        }
+            },500);
+      }
     }
+
     const onrightclick = () =>{
         if(bln===true){
             bln=false;
-        slideWrapper.current.append(slideWrapper.current.firstChild);
-        slideWrapper.current.style.transition="all 0s ease 0s";
-        slideWrapper.current.style.left=(originLeft+50)+"%";
-        originLeft+=50;
-        setTimeout(function(){
-            slideWrapper.current.style.transition="all 0.35s ease 0s";
-            slideWrapper.current.style.left=(originLeft-50)+"%";
-            originLeft-=50;
-        },10);
-        setTimeout(function(){
-            bln=true;
-        },200);
+            slideWrapper.current.append(slideWrapper.current.firstChild);
+            slideWrapper.current.style.transition="all 0s ease 0s";
+            slideWrapper.current.style.left=(originLeft+760)+"px";
+            originLeft+=760;
+            setTimeout(function(){
+                slideWrapper.current.style.transition="all 0.35s ease 0s";
+                slideWrapper.current.style.left=(originLeft-760)+"px";
+                originLeft-=760;
+            },10);
+            setTimeout(function(){
+                bln=true;
+            },500);
       }
     }
+
+    
+    const onplaystop = e => {
+      if(e.target.className==="controlBtn play"){
+        e.target.classList.remove("play");
+        e.target.classList.add("stop");
+      }
+      else{
+        e.target.classList.remove("stop");
+        e.target.classList.add("play");
+      }
+    }
+
 
     return (
       <div className="HomeMain">
         <div className="HomeMain__mainSlide">
-          <ul className="slideWrapper" ref={slideWrapper}>
+          <ul className="HomeMain__mainSlide__slideWrapper" ref={slideWrapper}>
             <li><img src={require('../asset/banner/001.jpg')} alt="banner001"></img></li>
             <li><img src={require('../asset/banner/002.jpg')} alt="banner002"></img></li>
             <li><img src={require('../asset/banner/003.jpg')} alt="banner003"></img></li>
             <li><img src={require('../asset/banner/004.jpg')} alt="banner004"></img></li>
             <li><img src={require('../asset/banner/005.jpg')} alt="banner005"></img></li>
+            <li><img src={require('../asset/banner/006.jpg')} alt="banner006"></img></li>
+            <li><img src={require('../asset/banner/007.jpg')} alt="banner007"></img></li>
           </ul>
           <button className="prevBtn" onClick={onleftclick}></button>
           <button className="nextBtn" onClick={onrightclick}></button>
+          <div className="HomeMain__mainSlide__indicator">
+            <button className="controlBtn play" onClick={onplaystop}></button>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
         </div>
       </div>
     );
--- src/components/MainNav.js
+++ src/components/MainNav.js
@@ -7,26 +7,30 @@ const MainNav = ({ mainEl, onClick}) => {
   
     return (
       <header>
-      <div className="headBar">
-        <ul>
-          <li>로그인</li>
-          <li>이용권</li>
-          <li>편성표</li>
-          <li>이벤트</li>
-        </ul>
-      </div>
-      <div className="mainNav">
-        <h1><img src={logo} alt="logo" onClick={onClick} data-num="01"></img></h1>
-        <ul className="mainNav__category" ref={mainEl}>
-          <li className="clicked" onClick={onClick} data-num="01">홈</li>
-          <li onClick={onClick} data-num="02">방송</li>
-          <li onClick={onClick} data-num="03">LIVE</li>
-          <li onClick={onClick} data-num="04">영화</li>
-          <li onClick={onClick} data-num="05">해외시리즈</li>
-        </ul>
-        <button className="mainNav__searchBtn"><img src={searchImg} alt="search"></img><span>검색하기</span></button>
-        <button className="mainNav__catBtn"><img src={catImg} alt="search"></img><span>카테고리 검색</span></button>
-      </div>
+        <div className="headBar">
+          <div className="headBar__wrapper">
+            <ul>
+              <li>로그인</li>
+              <li>이용권</li>
+              <li>편성표</li>
+              <li>이벤트</li>
+            </ul>
+          </div>
+        </div>
+        <div className="mainNav">
+          <div className="mainNav__wrapper">
+            <h1><img src={logo} alt="logo" onClick={onClick} data-num="01"></img></h1>
+            <ul className="mainNav__category" ref={mainEl}>
+              <li className="clicked" onClick={onClick} data-num="01">홈</li>
+              <li onClick={onClick} data-num="02">방송</li>
+              <li onClick={onClick} data-num="03">LIVE</li>
+              <li onClick={onClick} data-num="04">영화</li>
+              <li onClick={onClick} data-num="05">해외시리즈</li>
+            </ul>
+            <button className="mainNav__searchBtn"><img src={searchImg} alt="search"></img><span>검색하기</span></button>
+            <button className="mainNav__catBtn"><img src={catImg} alt="search"></img><span>카테고리 검색</span></button>
+          </div>
+        </div>
       </header>
     );
 };
